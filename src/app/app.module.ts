bb31c31637e474b6021067fec0c088df6277f5f3
= <=>
2022-02-18 19:06:32 +0100

    install ngrx + model + actions +reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Ngrx
+import { StoreModule } from '@ngrx/store';
+import { todoReducer } from './todos/todo.reducer';
+
 import { AppComponent } from './app.component';
 import { TodoModule } from './todos/todo.module';
 import { FooterComponent } from './footer/footer.component';
 
-
-
 @NgModule({
   declarations: [AppComponent, FooterComponent],
-  imports: [BrowserModule, TodoModule],
+  imports: [
+    BrowserModule,
+    TodoModule,
+    StoreModule.forRoot({ todos: todoReducer }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/todos/todo.reducer.ts
@@ -0,0 +1,14 @@
+import { createReducer, on } from '@ngrx/store';
+import { crear } from './todo.actions';
+import { Todo } from './models/todo.models';
+
+export const estadoInicial: Todo[] = [];
+
+const _todoReducer = createReducer(
+  estadoInicial,
+  on(crear, (state, { texto }) => [...state, new Todo(texto)])
+);
+
+export function todoReducer(state, action) {
+  return _todoReducer(state, action);
+}
