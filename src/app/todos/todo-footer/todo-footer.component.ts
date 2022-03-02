46d1738928710cf42dfc9400adc64bf4600e5772
= <=>
2022-03-02 19:52:44 +0100

    cambiamos el filtro al hacer click

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import * as actions from '../../filtro/filtro.actions';
 
 @Component({
   selector: 'app-todo-footer',
   templateUrl: './todo-footer.component.html',
-  styleUrls: ['./todo-footer.component.css']
+  styleUrls: ['./todo-footer.component.css'],
 })
 export class TodoFooterComponent implements OnInit {
+  filtroActual: actions.filtrosValidos = 'todos';
+  filtros: actions.filtrosValidos[] = ['todos', 'completados', 'pendientes'];
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.store
+      .select('filtro')
+      .subscribe((filtro) => (this.filtroActual = filtro));
   }
 
+  public cambiarFiltro(filtro: actions.filtrosValidos): void {
+    this.store.dispatch(actions.setFiltro({ filtro }));
+  }
 }
